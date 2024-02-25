709431c71ae440794aab3f7f4abdff5ddc11c290
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2024-02-25 17:51:42 -0300

    feat(link): Adiona rotas de Link

--- route/api.route.go
+++ route/api.route.go
@@ -3,15 +3,17 @@ package routes
 import (
 	API "url-shorting/api"
 	"url-shorting/middleware"
+	"url-shorting/resource/link"
 	"url-shorting/resource/user"
 )
 
 func Routes() {
 	api := API.New().GetInstance()
 
-	user.CreateRoutes()
+	user.CreateRoutes()	
 
 	api.Use(middleware.Auth)
 	user.CreatePrivateRoutes()
+	link.CreateRoutes()
 
 }
