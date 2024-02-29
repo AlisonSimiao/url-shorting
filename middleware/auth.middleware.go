83192bb35307a08570daeadd8f12640a7817d19d
Alison Simião <alison.s@multiplier.com.br>
2024-02-29 18:28:53 -0300

    fix(user): faz uso do cookie na autenticação

--- middleware/auth.middleware.go
+++ middleware/auth.middleware.go
@@ -2,21 +2,22 @@ package middleware
 
 import (
 	"url-shorting/token"
-
 	"github.com/gin-gonic/gin"
 )
 
 func Auth(c *gin.Context) {
 
-	t, errToken := token.ExtractJWTToken(c.GetHeader("Authorization"))
+	auth, _ := c.Cookie("auth_token")
+	
+	t, errToken := token.ExtractJWTToken(auth)
 	if errToken != "" {
 		c.AbortWithStatusJSON(401, gin.H{"error": "Falha na autenticação", "message": errToken})
 		return
 	}
-
+	
 	payload, err := token.VerifyToken(t)
 	if err != nil {
-		c.AbortWithStatusJSON(401, gin.H{"message": "Unauthorized"})
+		c.AbortWithStatusJSON(401, gin.H{"error": "Falha na autenticação", "message": "Unauthorized"})
 		return
 	}
 
