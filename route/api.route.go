96ec0f4b310830dbc8bf07224aa219778ad5540e
Alison Simião <alison.s@multiplier.com.br>
2024-04-22 18:52:36 -0300

    fix: add route version

--- route/api.route.go
+++ route/api.route.go
@@ -5,12 +5,17 @@ import (
 	"url-shorting/middleware"
 	"url-shorting/resource/link"
 	"url-shorting/resource/user"
+
+	"github.com/gin-gonic/gin"
 )
 
 func Routes() {
 	api := API.New().GetInstance()
 
-	user.CreateRoutes()	
+	api.GET("/", func(c *gin.Context) {
+		c.JSON(200, map[string]interface{}{"version": "1.0.0"})
+	})
+	user.CreateRoutes()
 
 	api.Use(middleware.Auth)
 	user.CreatePrivateRoutes()
