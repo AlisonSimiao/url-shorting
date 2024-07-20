6d04e94b071f3c80c17cd6f12da70ed2026531c3
Alison Simião <alison.s@multiplier.com.br>
2024-07-20 01:24:54 -0300

    docs: create swagger

--- main.go
+++ main.go
@@ -8,6 +8,21 @@ import (
 
 	"github.com/joho/godotenv"
 )
+// @title Vagas API
+// @version 0.1
+// @description docs backend vagas SPA
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+// @securityDefinitions.apiKey JWT
+// @in header
+// @name authorization
+// @license.name MIT License
+// @license.url 
+// @host localhost:8080
+// @BasePath /
+// @schemes http
 
 func main() {
 	err := godotenv.Load(".env")
--- route/api.route.go
+++ route/api.route.go
@@ -4,9 +4,27 @@ import (
 	API "vagas-api/api"
 	"vagas-api/middleware"
 	"vagas-api/resource/user"
-
+	_ "vagas-api/docs"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+// @title TODO APIs
+// @version 1.0
+// @description Testing Swagger APIs.
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+// @securityDefinitions.apiKey JWT
+// @in header
+// @name token
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8081
+// @BasePath /api/v1
+// @schemes htpt
 
 func Routes() {
 	api := API.New().GetInstance()
@@ -14,8 +32,8 @@ func Routes() {
 	api.GET("/", func(c *gin.Context) {
 		c.JSON(200, map[string]interface{}{"version": "1.0.0"})
 	})
+	api.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	user.CreateRoutes()
-
 	api.Use(middleware.Auth)
 	user.CreatePrivateRoutes()
 }
